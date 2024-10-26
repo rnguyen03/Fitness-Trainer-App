f4b97ee25744400bf65a8e65d705fd379339e577
Ryan Nguyen <ryanvannguyen@gmail.com>
2024-10-26 19:14:56 -0400

    Initial Commit

---
+++ Fitness Trainer App/Fitness_Trainer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_Trainer_AppApp.swift
+//  Fitness Trainer App
+//
+//  Created by Ryan Nguyen on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_Trainer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
